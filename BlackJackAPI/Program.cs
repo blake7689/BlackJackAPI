bf7d8076edb8c5b702f339f5186e27aab2ee998d
blake7689 <blakec7689@gmail.com>
2025-09-17 15:39:58 +0100

    Migration updates

--- BlackJackAPI/Program.cs
+++ BlackJackAPI/Program.cs
@@ -36,8 +36,6 @@ builder.Services.AddSwaggerGen();
 // Repositories
 builder.Services.AddScoped<IPlayerRepository, PlayerRepository>();
 
-Console.WriteLine("in api");
-
 // CORS
 builder.Services.AddCors(options =>
 {
